3db958754d1e1d06e96914e3b1e057b846201b46
Raphael Serafim <raphaelvserafim@gmail.com>
2023-01-12 17:22:19 -0400

    startStep

--- src/Flow.php
+++ src/Flow.php
@@ -8,27 +8,47 @@ class Flow extends DB
 {
 
     public $phone;
+    public $dateTimeNow;
+    public $serviceID;
 
+
+    public function __construct()
+    {
+        $this->dateTimeNow = date('Y-m-d H:i:s');
+    }
 
 
     public function checkService()
     {
-        return   DB::Select("SELECT * FROM tb_attendance WHERE phone  = '$this->phone' ");
+        return   DB::Select("SELECT * FROM tb_attendance WHERE phone  = '$this->phone' AND situation = 1 ");
     }
 
     public function startService()
     {
+        return   DB::Query("INSERT INTO  tb_attendance SET  phone  = '$this->phone', situation = 1 ");
     }
+
 
     public function endService()
     {
+        return   DB::Query("UPDATE tb_attendance SET situation = 0 WHERE phone  = '$this->phone' AND situation = 1 ");
     }
 
-    public function changeStage()
-    {
-    }
+
 
     public function stage()
     {
+        return   DB::Select("SELECT * FROM tb_stage WHERE attendance  =  $this->serviceID  ");
+    }
+
+
+    public function startStep()
+    {
+        return   DB::Query("INSERT INTO  tb_stage SET  attendance  = $this->serviceID , stage = 1 ");
+    }
+
+    public function changeStage(int $stage)
+    {
+        return   DB::Query("UPDATE tb_stage SET  stage  = $stage  WHERE attendance  = $this->serviceID  ");
     }
 }
