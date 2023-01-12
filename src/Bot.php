3a36137232eef7aeb400247a271082df28468e41
Raphael Serafim <raphaelvserafim@gmail.com>
2023-01-12 17:55:11 -0400

    up

--- src/Bot.php
+++ src/Bot.php
@@ -6,58 +6,11 @@ use NLTK\NLTK;
 use Rasa\Rasa;
 use SentimentAnalysis\SentimentAnalysis;
 
-use Cachesistemas\ChatbotWhatsappPhp\DB;
 
-class Bot extends DB
+class Bot
 {
     public $wpp_server;
     public $wpp_key;
-    public $phone;
-    public $dateTimeNow;
-    public $serviceID;
-    public $stageID;
-
-
-    public function __construct()
-    {
-        $this->dateTimeNow = date('Y-m-d H:i:s');
-    }
-
-
-    public function checkService()
-    {
-        return   DB::Select("SELECT * FROM tb_attendance WHERE phone  = '$this->phone' AND situation = 1 ");
-    }
-
-    public function startService()
-    {
-        return   DB::Query("INSERT INTO  tb_attendance SET  phone  = '$this->phone', situation = 1 ");
-    }
-
-
-    public function endService()
-    {
-        return   DB::Query("UPDATE tb_attendance SET situation = 0 WHERE phone  = '$this->phone' AND situation = 1 ");
-    }
-
-
-    public function stage()
-    {
-        return   DB::Select("SELECT * FROM tb_stage WHERE attendance  =  $this->serviceID  ");
-    }
-
-
-    public function startStep()
-    {
-        return   DB::Query("INSERT INTO  tb_stage SET  attendance  = $this->serviceID , stage = 1 ");
-    }
-
-    public function changeStage(int $stage)
-    {
-        return   DB::Query("UPDATE tb_stage SET  stage  = $stage  WHERE attendance  = $this->serviceID  ");
-    }
-
-
 
 
 
