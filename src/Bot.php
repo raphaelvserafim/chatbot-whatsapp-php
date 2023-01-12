7aab380555dc2065c712574ed40631b46e37e4cc
Raphael Serafim <raphaelvserafim@gmail.com>
2023-01-12 16:29:52 -0400

    NLTK & Rasa

--- src/Bot.php
+++ src/Bot.php
@@ -2,6 +2,10 @@
 
 namespace Cachesistemas\ChatbotWhatsappPhp;
 
+use NLTK\NLTK;
+
+use Rasa\Rasa;
+
 
 class Bot
 {
@@ -9,7 +13,21 @@ class Bot
     public $wpp_key;
 
 
-    
-  
+    public function NLTK()
+    {
 
+        $nltk = new NLTK();
+        $text = "Barack Obama was born in Hawaii and was the 44th President of the United States.";
+        $entities = $nltk->ner()->extract($text);
+        print_r($entities);
+    }
+
+    public function Rasa()
+    {
+
+        $rasa = new Rasa();
+        $text = "I am looking for a cheap hotel in Paris";
+        $intents = $rasa->nlu()->parse($text);
+        print_r($intents);
+    }
 }
