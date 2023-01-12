ab06df21797b14f5a41391edbc5b92d863905ee1
Raphael Serafim <raphaelvserafim@gmail.com>
2023-01-12 16:46:43 -0400

    DB

--- src/DB.php
+++ src/DB.php
@@ -1,3 +1,55 @@
 <?php
 
 namespace Cachesistemas\ChatbotWhatsappPhp;
+ 
+
+ 
+use PDO;
+use PDOException;
+
+
+
+class DB
+{
+
+    private  function conectar()
+    {
+
+        try {
+            $conect  = new PDO("mysql:host=" . DB_HOST  . ";dbname=" . DB_BASE_NAME . "", DB_USER, DB_PASS);
+
+            $conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conect->exec("SET NAMES 'utf8';");
+        } catch (PDOException $e) {
+            echo json_encode(array("status" => false, "mensagem" => $e->getMessage()));
+            die();
+        }
+        return  $conect;
+    }
+    
+
+    public function Query($sql)
+    {
+
+        try {
+            $stmt = $this->conectar()->prepare($sql);
+            $stmt->execute();
+            return  array("status" => true);
+        } catch (PDOException $e) {
+            return array("status" => false, "mensagem" =>  $e->getMessage());
+        }
+    }
+
+    public  function Select($sql)
+    {
+
+        try {
+            $stmt = $this->conectar()->prepare($sql);
+            $stmt->execute();
+            return array("status" => true,  "consulta" => $stmt->fetchAll(PDO::FETCH_OBJ));
+        } catch (PDOException $e) {
+
+            return array("status" => false, "mensagem" =>  $e->getMessage());
+        }
+    }
+}
