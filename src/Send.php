fe3e8d3d4ad2f86d499adfd3d3a55db72391dbe1
Raphael Serafim <raphaelvserafim@gmail.com>
2023-01-13 08:01:17 -0400

    WhatsApp

--- src/Send.php
+++ src/Send.php
@@ -2,12 +2,21 @@
 
 namespace Cachesistemas\ChatbotWhatsappPhp;
 
+use Cachesistemas\ClassePhpApiWame\WhatsApp;
+
 
 class Send
 {
+    public $wpp_server;
+    public $wpp_key;
 
     public function whatsApp($msg)
     {
+
+        $whatsapp     = new WhatsApp(["server" =>  $this->wpp_server, "key" =>  $this->wpp_key]);
+
+
+        
         if (sizeof($msg) > 0) {
         }
     }
